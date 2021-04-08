987102277fec0ac86f944e94b6027c26dfbb03fd
Allan Alves <allanalvesmail@gmail.com>
2021-04-07 22:47:38 -0300

    Configure spring cloud resilience4j circuitbreaker

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.devsuperior.hrpayroll.resources;
 
 import com.devsuperior.hrpayroll.entities.*;
 import com.devsuperior.hrpayroll.services.*;
+import io.github.resilience4j.circuitbreaker.annotation.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +13,16 @@ public class PaymentResource {
 
   @Autowired private PaymentService paymentService;
 
+  @CircuitBreaker(name = "getPayment", fallbackMethod = "getPaymentAlternative")
   @GetMapping("/{workerId}/days/{days}")
   public ResponseEntity<Payment> getPayment(
       @PathVariable Long workerId, @PathVariable Integer days) {
     Payment payment = paymentService.getPayment(workerId, days);
+    return ResponseEntity.ok(payment);
+  }
+
+  private ResponseEntity<Payment> getPaymentAlternative(Throwable t) {
+    Payment payment = new Payment("Bran", 400.00, 2);
     return ResponseEntity.ok(payment);
   }
 }
