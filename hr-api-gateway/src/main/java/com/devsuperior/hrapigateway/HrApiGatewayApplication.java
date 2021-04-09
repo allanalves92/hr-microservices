6d20ec9e6c85f0e54c078ee3ad079070d26f8513
Allan Alves <allanalvesmail@gmail.com>
2021-04-09 19:44:24 -0300

    Configuring cloud api gateway

--- hr-api-gateway/src/main/java/com/devsuperior/hrapigateway/HrApiGatewayApplication.java
+++ hr-api-gateway/src/main/java/com/devsuperior/hrapigateway/HrApiGatewayApplication.java
@@ -1,13 +1,14 @@
 package com.devsuperior.hrapigateway;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.cloud.netflix.eureka.*;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrApiGatewayApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(HrApiGatewayApplication.class, args);
+  }
 }
