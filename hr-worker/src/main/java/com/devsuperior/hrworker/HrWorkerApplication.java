6804e80f029bdef9dd44741ff182d0e8369d0ca5
Allan Alves <allanalvesmail@gmail.com>
2021-04-12 20:42:44 -0300

    Hr-Worker as a config server client

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.cloud.netflix.eureka.*;
 
-@SpringBootApplication
-@EnableEurekaClient
+@SpringBootApplication()
+@EnableEurekaClient()
 public class HrWorkerApplication {
 
   public static void main(String[] args) {
--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -2,6 +2,7 @@ package com.devsuperior.hrworker.resources;
 
 import com.devsuperior.hrworker.entities.*;
 import com.devsuperior.hrworker.repositories.*;
+import lombok.extern.slf4j.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -10,20 +11,20 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/workers")
+@Slf4j
 public class WorkerResource {
 
-    @Autowired
-    private WorkerRepository repository;
+  @Autowired private WorkerRepository repository;
 
-    @GetMapping
-    public ResponseEntity<List<Worker>> findAll() {
-        List<Worker> list = repository.findAll();
-        return ResponseEntity.ok(list);
-    }
+  @GetMapping
+  public ResponseEntity<List<Worker>> findAll() {
+    List<Worker> list = repository.findAll();
+    return ResponseEntity.ok(list);
+  }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Worker> findById(@PathVariable Long id) {
-        Worker worker = repository.findById(id).get();
-        return ResponseEntity.ok(worker);
-    }
+  @GetMapping("/{id}")
+  public ResponseEntity<Worker> findById(@PathVariable Long id) {
+    Worker worker = repository.findById(id).get();
+    return ResponseEntity.ok(worker);
+  }
 }
