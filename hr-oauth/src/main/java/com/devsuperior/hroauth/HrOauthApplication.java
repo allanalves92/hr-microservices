aedf0fb0f5b46342b9b705b285adc37c2c2985db
Allan Alves <allanalvesmail@gmail.com>
2021-04-13 21:23:26 -0300

    Configuring hr-oauth project

--- hr-oauth/src/main/java/com/devsuperior/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/HrOauthApplication.java
@@ -1,13 +1,14 @@
 package com.devsuperior.hroauth;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.cloud.netflix.eureka.*;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrOauthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrOauthApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(HrOauthApplication.class, args);
+  }
 }
